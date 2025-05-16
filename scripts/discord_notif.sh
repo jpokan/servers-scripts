88dde5cf7aeaae26a819fdc23bc1b7957abc3105
jpokan <jorgeou1993@gmail.com>
2025-05-16 22:47:18 +0900

    fix: clean up

--- scripts/discord_notif.sh
+++ scripts/discord_notif.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WEBHOOK_URL="https://discord.com/api/webhooks/1368547031450390570/f9aZcNA1ixgrRFaR7-DDAK8T6NCdD8fC5OfZ5yES7WBJ1komcc8C3Vxbuq_DoSQsFFdQ"
+. ./.env #WEBHOOK_URL
 
 discord_backed() {
 	BODY="{\"username\": \"Server\", \"content\": \"> **Express** - \`backup_trigger\` - Backed Up! - file: ||nc_backup_$DATE.db|| \"}"
